47555b7b4d53765b07a8d3383ee1d7d50575f431
Beata Markunas <beata.markunas@gmail.com>
2018-04-16 19:43:26 +0200

    Define url

--- components/Search.js
+++ components/Search.js
@@ -1,4 +1,26 @@
 Search = React.createClass({
+    
+    getInitialState() {
+        return {
+            searchingText: ''
+        };
+    },
+
+    handleChange: function(event) {
+        var searchingText = event.target.value;
+        this.setState({searchingText: searchingText});
+
+        if (searchingText.length > 2) {
+          this.props.onSearch(searchingText);
+        }
+    },
+
+    handleKeyUp: function(event) {
+        if (event.keyCode === 13) {
+          this.props.onSearch(this.state.searchingText);
+        }
+    },
+
     render: function() {
         var styles = {
             fontSize: '1.5em',
@@ -13,5 +35,6 @@ Search = React.createClass({
              style={styles}
              value={this.state.searchTerm}
             />
-  }
+        }
+     }   
 });
